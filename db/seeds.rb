90e16621cd51280e111533498c2625cb23e40317
bitKangaroo3 <akakangaroo@gmail.com>
2019-03-07 19:40:58 +0900

    write seed for Product

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#User.create!(email: "test@test.com",
+#						password: "foober")
+
+6.times do |p|
+	name = Faker::Commerce.product_name
+	description = Faker::Lorem.paragraph(2)
+	price = Faker::Number.number(5)
+	units = ["yen", "dollar"]
+	unit = units[rand(2)]
+
+	Product.create!(name: name,
+									description: description,
+									price: price,
+									unit: unit)
+end
